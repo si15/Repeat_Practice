ea7856b190f7fca9baa58238e55a3f341677a968
si15 <si09@naver.com>
2019-01-26 11:48:13 +0900

    별찍기 정답

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -54,7 +54,18 @@ public class MainDrive {
 //		*****
 //		*****
 		
-		
+//		세로 줄바꿈
+		for (int i = 0; i < 5; i++) {
+			
+////			가로 별찍기
+//			for (int j = 0; j < 5; j++) {
+//				System.out.print("*");
+//				
+//			}
+//			
+//			System.out.println();
+//			
+//		}
 
 		
 	}
